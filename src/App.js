d059a64ae9b485e47ea61afd86f7ff5a038fd2c2
Maiko <maikossmaster@gmail.com>
2018-05-23 16:18:22 -0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './styles/global';
+
+const App = () => (
+  <div className="App">
+    <h1>Welcome to React</h1>
+  </div>
+);
+
+export default App;
---
+++ src/styles/global.js
@@ -0,0 +1,17 @@
+import { injectGlobal } from 'styled-components';
+
+injectGlobal`
+
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body{
+    background: #9B65E5;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important; 
+  }
+
+`;
